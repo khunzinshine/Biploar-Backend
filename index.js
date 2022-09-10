c199d56f100a399fad2424bfa21fa4e586383c7f
khunzinshine <khunzinshine@ayainnovation.com>
2022-09-10 20:54:23 +0630

    fixed deploy

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get("/", (req, res) => {
-  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-})
+// app.get("/", (req, res) => {
+//   res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+// })
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
