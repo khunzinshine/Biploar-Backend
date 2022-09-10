805ad2ddd15111b2b1974498e648a2d81421db7e
khunzinshine <khunzinshine@ayainnovation.com>
2022-09-10 20:07:11 +0630

    set up vercel deploy config

--- index.js
+++ index.js
@@ -28,11 +28,16 @@ mongoose.connection.on("disconnected", () => {
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+
+app.get("/", (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+})
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
